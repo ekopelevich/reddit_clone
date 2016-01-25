6898e41a0b97551e455cc5182156ef7059f9bcc2
Elana Kopelevich <ekopelevich@gmail.com>
2016-01-24 18:18:09 -0700

    adding new posts to local storage

--- controllers.js
+++ controllers.js
@@ -2,13 +2,16 @@ var app = angular.module('redditApp', []);
 
 app.controller('RedditController', function($scope, $http){
 
-  function Post(image, score, title, author, date, comments) {
-    this.image = image;
-    this.score = score;
-    this.title = title;
+  $scope.posts = [];
+
+  function Post(author, title, date, description, image, score, comments) {
     this.author = author;
+    this.title = title;
     this.date = date;
+    this.description = description;
+    this.image = image;
     this.comments = comments;
+    this.score = score;
     this.upVote = function(){
       this.score++;
     };
@@ -24,52 +27,42 @@ app.controller('RedditController', function($scope, $http){
   //   date = this.date;
   // }
 
-  $http.get('https://www.reddit.com/r/earthporn.json')
-        .then(function successCallback(response){
-          console.log(response);
+  if (!localStorage.getItem('posts')) {
 
-          $scope.posts = [];
+    var response = $http.get('https://www.reddit.com/r/earthporn.json')
+      .then(function successCallback(response){
+        console.log(response);
 
-          var listings = response.data.data.children;
-          var post;
+        $scope.posts = [];
 
-          for (var i = 0; i < listings.length; i++) {
-            post = new Post(listings[i].data.thumbnail, listings[i].data.score, listings[i].data.title, listings[i].data.author, listings[i].data.created_utc, listings[i].data.num_comments);
+        var listings = response.data.data.children;
+        var post;
 
-            $scope.posts.push(post);
-          }
-          console.log($scope.posts);
-        }, function errorCallback(response) {
-          console.log(response, error);
-        });
+        for (var i = 0; i < listings.length; i++) {
+          post = new Post(listings[i].data.author, listings[i].data.title,  listings[i].data.created_utc, 'description', listings[i].data.thumbnail, listings[i].data.score, listings[i].data.num_comments);
 
+          $scope.posts.push(post);
+          localStorage.setItem('posts', JSON.stringify($scope.posts));
+        }
+      }, function errorCallback(response) {
+        console.log(response, error);
+      });
+  } else {
+    $scope.posts = localStorage.getItem('posts');
+  }
 
+  $scope.newPost = function(){
 
+    var post = new Post(newPostForm.author.value, newPostForm.title.value, Date.now().value, newPostForm.description.value, newPostForm.image.value, 0, 0);
 
+    $scope.posts = JSON.parse(localStorage.getItem('posts'));
+    $scope.posts.push(post);
+    localStorage.setItem('posts', JSON.stringify($scope.posts));
+    $window.location.href = '/';
+  };
 
-  // $scope.comments = [
-  //   {
-  //     author: 'Billy Idol',
-  //     date: 342343453452253,
-  //     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dolor elit, sodales in magna nec, tempor accumsan purus. Suspendisse potenti. Mauris sapien magna, luctus in hendrerit quis, pretium hendrerit eros.',
-  //   }
-  // ];
-  //
-  // $scope.posts = [
-  //   {
-  //     title: 'Look at this cute puppy!',
-  //     author: 'Billy Idol',
-  //     date: 342343453452253,
-  //     image: './images/puppy.jpg',
-  //     comments: $scope.comments.length,
-  //     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dolor elit, sodales in magna nec, tempor accumsan purus. Suspendisse potenti. Mauris sapien magna, luctus in hendrerit quis, pretium hendrerit eros.',
-  //     votes: $scope.upvotes - $scope.downvotes
-  //   }
-  // ];
+  $scope.viewComments = function(){
 
-
-});
-
-app.controller('newPostController', function($scope){
+  };
 
 });
